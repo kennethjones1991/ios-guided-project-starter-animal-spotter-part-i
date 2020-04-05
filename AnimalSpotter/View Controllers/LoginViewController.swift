eac1ad3441e1a701aa68b5af8a6a46360d9f6fc5
Scott Gardner <scott.gardner@mac.com>
2020-04-05 10:13:32 -0500

    Add activity indicator to login view controller

--- AnimalSpotter/View Controllers/LoginViewController.swift
+++ AnimalSpotter/View Controllers/LoginViewController.swift
@@ -22,6 +22,7 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     var loginType = LoginType.signUp
     
